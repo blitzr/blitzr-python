c4ad801ffa87a23a1efda448893224bd794fdd3b
Pascal <pascal@blitzr.com>
2016-10-07 14:16:18 +0200

    implement auto doc building

---
+++ update-doc.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+shopt -s extglob # enable extglob shell option to use extended pattern matching operators
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+function doCompile {
+    pip install Sphinx
+    sphinx-build source docs
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just building doc."
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+
+pip install Sphinx
+
+sphinx-build source docs
+
+rm -rf !(docs|.git|deploy_key.enc)
+
+cp -R docs/* .
+
+rm -rf docs
+
+touch .nojekyll
+
+
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# if [ -z `git diff --exit-code` ]; then
+#     echo "No changes to the output on this push; exiting."
+#     exit 0
+# fi
+
+git add -A .
+git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+# Now that we're all set up, we can push.
+git push $SSH_REPO $TARGET_BRANCH
